07f2097956d23495ed63e5ac2216cbbdbf1ae9b1
Halvor Holsten Strand <halvor.holsten.strand@gmail.com>
2019-10-02 23:06:05 +0200

    Refactored functions with async/await to clean up a bit.

--- lib/sbanken.ts
+++ lib/sbanken.ts
@@ -2,76 +2,44 @@ import { request, proxy } from './request';
 import btoa from 'btoa';
 import { SbankenAccessToken, SbankenAccounts } from './models';
 
-export const getAccessToken = (userId: number, clientId: string, clientSecret: string): Promise<SbankenAccessToken> => {
+export const getAccessToken = async (userId: number, clientId: string, clientSecret: string): Promise<SbankenAccessToken> => {
   var identityServerUrl = "https://auth.sbanken.no/identityserver/connect/token";
   var basicAuth = btoa(encodeURIComponent(clientId) + ":" + encodeURIComponent(clientSecret));
 
-  var promise = new Promise<SbankenAccessToken>(function (resolve, reject) {
-    request
-      .post(identityServerUrl)
-      .proxy(proxy)
-      .send('grant_type=client_credentials')
-      .set('Authorization', "Basic " + basicAuth)
-      .set('Accept', 'application/json')
-      .set('customerId', '' + userId)
-      .end(function (err: any, res: any) {
-        if (err || !res.ok) {
-          reject(err);
-        } else {
-          resolve(res.body);
-        }
-      });
-  });
-
-  return promise;
+  return request
+    .post(identityServerUrl)
+    .proxy(proxy)
+    .send('grant_type=client_credentials')
+    .set('Authorization', "Basic " + basicAuth)
+    .set('Accept', 'application/json')
+    .set('customerId', '' + userId)
+    .then((result: any) => result.body);
 }
 
-export const performTransaction = (userId: number, accessToken: string, fromAccountId: string, toAccountId: string, message: string, amount: number): Promise<boolean> => {
+export const performTransaction = async (userId: number, accessToken: string, fromAccountId: string, toAccountId: string, message: string, amount: number): Promise<boolean> => {
   var identityServerUrl = "https://api.sbanken.no/exec.bank/api/v1/transfers/";
 
-  var promise = new Promise<boolean>(function (resolve, reject) {
-    request
-      .post(identityServerUrl)
-      .proxy(proxy)
-      .set('Authorization', "Bearer " + accessToken)
-      .set('customerId', '' + userId)
-      .send({
-        fromAccountId,
-        toAccountId,
-        message,
-        amount,
-      })
-      .end(function (err: any, res: any) {
-        if (err || !res.ok) {
-          reject(err);
-        } else {
-          resolve(true);
-        }
-      });
-  });
-
-  return promise;
+  return request
+    .post(identityServerUrl)
+    .proxy(proxy)
+    .set('Authorization', "Bearer " + accessToken)
+    .set('customerId', '' + userId)
+    .send({
+      fromAccountId,
+      toAccountId,
+      message,
+      amount,
+    }).then((result: any) => true);
 }
 
-export const getAccountDetails = (userId: number, accessToken: string): Promise<SbankenAccounts> => {
+export const getAccountDetails = async (userId: number, accessToken: string): Promise<SbankenAccounts> => {
   var accountServiceUrl = "https://api.sbanken.no/exec.bank/api/v1/accounts/";
 
-  var promise = new Promise<SbankenAccounts>(function (resolve, reject) {
-    request
-      .get(accountServiceUrl)
-      .proxy(proxy)
-      .set('Authorization', "Bearer " + accessToken)
-      .set('Accept', 'application/json')
-      .set('customerId', '' + userId)
-      .end(function (err: any, res: any) {
-        if (err || !res.ok) {
-          console.log(err, res);
-          reject(err);
-        } else {
-          resolve(res.body);
-        }
-      });
-  });
-
-  return promise;
+  return request
+    .get(accountServiceUrl)
+    .proxy(proxy)
+    .set('Authorization', "Bearer " + accessToken)
+    .set('Accept', 'application/json')
+    .set('customerId', '' + userId)
+    .then((result: any) => result.body);
 }
--- lib/yr.ts
+++ lib/yr.ts
@@ -2,37 +2,23 @@ import { request, proxy } from './request';
 import { Parser } from 'xml2js';
 import { Yr } from './models';
 
-export const getForecast = (placeUrl: string): Promise<Yr> => {
-    var promise = new Promise<Yr>(function (resolve, reject) {
-        request.get(`${placeUrl}/varsel.xml`)
-            .proxy(proxy)
-            .end(function (err: any, res: any) {
-                if (err || !res.ok) {
-                    reject(err);
-                } else {
-                    const parser = new Parser({ explicitArray: false });
-                    parser.parseStringPromise(res.text).then((response) => {
-                        resolve(response);
-                    });
-                }
-            });
-    });
-
-    return promise;
+export const getForecast = async (placeUrl: string): Promise<Yr> => {
+    return request.get(`${placeUrl}/varsel.xml`)
+        .proxy(proxy)
+        .then((result: any) => {
+            const parser = new Parser({ explicitArray: false });
+            return parser.parseStringPromise(result.text);
+        });
 }
 
-export const getTodaysPrecipitation = (placeUrl: string): Promise<number> => {
-    return getForecast(placeUrl).then((response) => {
-        let sum: number = 0;
-        for (const time of response.weatherdata.forecast.tabular.time) {
-            if (new Date(Date.parse(time.$.from)).toDateString() == new Date().toDateString()) {
-                const { value } = time.precipitation.$;
-                sum += parseFloat(value);
-            }
+export const getTodaysPrecipitation = async (placeUrl: string): Promise<number> => {
+    const yr = await getForecast(placeUrl);
+    let sum: number = 0;
+    for (const time of yr.weatherdata.forecast.tabular.time) {
+        if (new Date(Date.parse(time.$.from)).toDateString() == new Date().toDateString()) {
+            const { value } = time.precipitation.$;
+            sum += parseFloat(value);
         }
-        return sum;
-    }).catch((reason: string) => {
-        console.error(reason);
-        return -1;
-    });
+    }
+    return sum;
 }
